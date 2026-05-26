0ec8bb4989581e5b552fb62c0c2baec70a25be9e
zaohon <1402767827@qq.com>
2026-05-26 10:08:50 +0800

    Use relative API base path

--- src/costants/costant.ts
+++ src/costants/costant.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_AGENT_ID = '59'
 
-export const API_BASE = 'http://localhost:3000'
+export const API_BASE = '/api'
 
 export const OSS_LOGO_BASE = 'https://lqwlcloud.oss-cn-shanghai.aliyuncs.com/system/agent-logo/'
 
